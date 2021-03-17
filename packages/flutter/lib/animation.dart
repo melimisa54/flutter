ed7dede6cff01eea6ff8c9720ab8df0c22a6dae1
melimisa54 <77334875+melimisa54@users.noreply.github.com>
2021-03-17 18:39:08 +0700

    Update animation.dart

--- packages/flutter/lib/animation.dart
+++ packages/flutter/lib/animation.dart
@@ -1,8 +1,4 @@
-// Copyright 2014 The Flutter Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
 
-/// The Flutter animation system.
 ///
 /// To use, import `package:flutter/animation.dart`.
 ///
